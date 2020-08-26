de64c8eaed00ac70bae07155ac86e85b9611fa76
bianca.barbosa <biadiasbarbosa@hotmail.com>
2020-08-25 23:18:54 -0300

    #5 To-Do list with firebase and local variables

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -5,6 +7,19 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: 'menuti-challenge',
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,10 +37,11 @@
               <v-checkbox
                 v-model="task.done"
                 :color="task.done && 'grey' || 'black'"
+                @click="update(task.id, task.done)"
               >
                 <template v-slot:label>
                   <div
-                    :class="task.done && 'grey--text' || 'black--text'"
+                    :class="task.done && 'grey--text text-decoration-line-through' || 'black--text'"
                     class="ml-4"
                     v-text="task.name"
                   ></div>
@@ -51,19 +52,53 @@
         </template>
       </v-slide-y-transition>
     </v-card>
+
+    <v-alert
+    v-else
+    type="info"
+    text
+    border="left"
+    >
+      Nenhuma atividade localizada.
+    </v-alert>
+
+    <v-snackbar
+      v-model="snackbar_add"
+      multi-line
+      timeout="2000"
+    >
+      Erro ao adicionar atividade.
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="red"
+          text
+          v-bind="attrs"
+          @click="snackbar_add = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
+import firebase from 'firebase/app';
+
 export default {
   name: 'Home',
   data: () => ({
-    tasks: [{
-      name: 'Example 1',
-      done: false,
-    }],
+    item: 1,
+    items: [
+      { text: 'Real-Time', icon: 'mdi-clock' },
+      { text: 'Audience', icon: 'mdi-account' },
+      { text: 'Conversions', icon: 'mdi-flag' },
+    ],
+    tasks: [],
     task: null,
+    db: firebase.firestore(),
+    snackbar_add: false,
   }),
 
   computed: {
@@ -76,16 +111,36 @@ export default {
   },
 
   methods: {
-    create() {
+    async create() {
       if (this.task) {
-        this.tasks.push({
-          done: false,
-          name: this.task,
-        });
-
-        this.task = null;
+        try {
+          await this.db.collection('tasks').add({
+            done: false,
+            name: this.task,
+            created_at: new Date().toLocaleString(),
+          });
+        } catch (error) {
+          this.snackbar_add = true;
+        }
       }
+      this.task = null;
+      this.getActivities();
     },
+    getActivities() {
+      this.db.collection('tasks').orderBy('created_at', 'asc').get()
+        .then((response) => {
+          this.tasks = response.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        });
+    },
+    async update(id, status) {
+      await this.db.collection('tasks').doc(id).update({
+        done: status,
+      });
+    },
+  },
+
+  created() {
+    this.getActivities();
   },
 };
 </script>
